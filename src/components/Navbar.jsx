43e7f758a501ddb693ed29824d6fe498c5cad22c
Kristian Utsar <utsaru@gmail.com>
2022-04-07 21:20:38 +0100

    adds dummy data and navbar boilerplate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { menuData } from "../assets/data/MenuData";
+
+const Nav = styled.nav`
+  height: 60px;
+  background: red;
+`;
+
+const Logo = styled(Link)``;
+const MenuBars = styled.i``;
+const NavMenu = styled.div``;
+const NavMenuLinks = styled(Link)``;
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Nav>
+      <Logo>Utsar-Living</Logo>
+      <MenuBars />
+      <NavMenu>
+        {menuData.map((item, index) => {
+          <NavMenuLinks to={item.link} key={index}>
+            {item.title}
+          </NavMenuLinks>;
+        })}
+      </NavMenu>
+    </Nav>
+  );
 };
 
 export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
