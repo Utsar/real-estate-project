d6dfaa86ca74e7fabcf529b687c501b7620e9364
Kristian Utsar <utsaru@gmail.com>
2022-04-11 21:46:01 +0100

    adds global styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,44 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components/macro";
 import { Link } from "react-router-dom";
 import { menuData } from "../assets/data/MenuData";
+import { Button } from "./Button";
 
 const Nav = styled.nav`
   height: 60px;
-  background: red;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  z-index: 1000;
+  position: fixed;
+  background: #000;
+  width: 100%;
+`;
+const NavLink = css`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  text-decoration: none;
 `;
 
-const Logo = styled(Link)``;
+const Logo = styled(Link)`
+  ${NavLink}
+  font-style: italic;
+`;
 const MenuBars = styled.i``;
 const NavMenu = styled.div`
-  background-color: teal;
+  display: flex;
+  align-items: center;
+  margin-right: -48px;
 `;
 const NavMenuLinks = styled(Link)`
-  color: white;
+  ${NavLink}
+`;
+const NavButton = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 const Navbar = () => {
@@ -22,12 +47,17 @@ const Navbar = () => {
       <Logo to="/">Utsar-Living</Logo>
       <MenuBars />
       <NavMenu>
-        {menuData.map((item, index) => {
+        {menuData.map((item, index) => (
           <NavMenuLinks to={item.link} key={index}>
             {item.title}
-          </NavMenuLinks>;
-        })}
+          </NavMenuLinks>
+        ))}
       </NavMenu>
+      <NavButton>
+        <Button to="/contact" primary={true}>
+          Contact Us
+        </Button>
+      </NavButton>
     </Nav>
   );
 };
