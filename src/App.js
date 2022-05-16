371e4982609397086eb06cfe8c9361fc199e01ee
Kristian Utsar <utsaru@gmail.com>
2022-05-16 23:18:17 +0100

    adds pages but confused about routing.... need sleep

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { useState } from "react";
-import { InfoData, InfoDataTwo } from "./assets/data/InfoData";
-import { SliderData } from "./assets/data/SliderData";
 import Dropdown from "./components/Dropdown";
-import Features from "./components/Features";
 import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import InfoSection from "./components/InfoSection";
-import Listings from "./components/Listings";
 import Navbar from "./components/Navbar";
 import GlobalStyle from "./globalStyles";
+
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 const App = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -20,11 +18,10 @@ const App = () => {
       <GlobalStyle />
       <Navbar toggle={toggle} />
       <Dropdown isOpen={isOpen} toggle={toggle} />
-      <Hero slides={SliderData} />
-      <InfoSection {...InfoData} />
-      <Listings />
-      <Features />
-      <InfoSection {...InfoDataTwo} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
       <Footer />
     </>
   );
